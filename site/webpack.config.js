cf3ba3d2430c4edc89160694526cd6687a009beb
Peter Elst <peter.elst@gmail.com>
2025-05-22 14:28:56 +0100

    initial commit for site

---
+++ site/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/app.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'manifest.json', to: '.' },
+        { from: 'sw.js', to: '.' },
+        { from: 'images', to: 'images' },
+        { from: 'audio', to: 'audio' },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js'],
+  },
+};
